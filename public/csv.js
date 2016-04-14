1f6eed688e34215caa7fa17c87e3f723cebe188d
Albano Yanes <ajyanreyu@gmail.com>
2016-04-14 22:28:48 +0100

    cambio en la localizacion de los archivos

--- public/csv.js
+++ public/csv.js
@@ -80,7 +80,7 @@ $(document).ready(() => {
      });
    /* botones para rellenar el textarea */
    $('button.example').each( (_,y) => {
-     $(y).click( () => { dump(`public/${$(y).text()}.txt`); });
+     $(y).click( () => { dump(`${$(y).text()}.txt`); });
    });
 
     // Setup the drag and drop listeners.
